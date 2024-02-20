3a4e9e7dbebe944aa6f2a57fa72693231a7723d0
206676 <joshcry23@gmail.com>
2024-02-20 10:14:35 +0800

    Added bullet point iteration

--- GISFirstDjango/views.py
+++ GISFirstDjango/views.py
@@ -3,6 +3,7 @@ from .models import Person
 
 def splash(request):
     name = 'Billy'
+    lst = ['Cat', 'Dog', 'Mouse', 'Monkey', 'Bird']
     people = Person.objects.all()
     debug_people = list(people)
-    return render(request, "splash.html", {'name': name})
+    return render(request, "splash.html", {'name': name,'lst':lst})
